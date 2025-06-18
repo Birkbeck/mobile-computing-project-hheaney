25aae45bab9345eeea3d7d63af97bdd1a66bb004
Harry Heaney <h.heaney@hotmail.co.uk>
2025-06-19 00:09:53 +0100

    RecipesDatabase file added

---
+++ app/src/main/java/co/uk/bbk/culinarycookingapp/RecipesDatabase.kt
@@ -0,0 +1,29 @@
+package co.uk.bbk.culinarycookingapp
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Recipe::class], version = 1)
+abstract class RecipesDatabase : RoomDatabase(){
+
+    abstract fun recipesDao(): RecipesDao
+
+    companion object {
+       @Volatile
+        private var INSTANCE: RecipesDatabase? = null
+        fun getInstance(context: Context): RecipesDatabase {
+            return INSTANCE ?: synchronized(this) {
+                INSTANCE ?: Room.databaseBuilder(
+                    context.applicationContext,
+                    RecipesDatabase::class.java,
+                    "recipes_database"
+                )
+                .fallbackToDestructiveMigration()
+                .build().also { INSTANCE = it }
+
+            }
+        }
+    }
+}
