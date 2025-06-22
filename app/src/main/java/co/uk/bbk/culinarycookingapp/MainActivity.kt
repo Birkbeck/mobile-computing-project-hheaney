f47102818ae6b096314e89758908d59653e235ad
Harry Heaney <h.heaney@hotmail.co.uk>
2025-06-22 22:37:27 +0100

    Initial recycler adapter class attempt.  Seems to work on a basic level but need to improve UI appearance.  Some overlapping issues.

--- app/src/main/java/co/uk/bbk/culinarycookingapp/MainActivity.kt
+++ app/src/main/java/co/uk/bbk/culinarycookingapp/MainActivity.kt
@@ -5,16 +5,38 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import co.uk.bbk.culinarycookingapp.databinding.ActivityAllRecipesBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAllRecipesBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityAllRecipesBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val recipes = listOf(
+            Recipe(
+                id = 1,
+                name = "Spaghetti Bolognese",
+                imageUri = R.drawable.chicken_curry
+            ),
+            Recipe(
+                id = 2,
+                name = "Chicken Curry",
+                imageUri = R.drawable.chicken_curry
+            ),
+            Recipe(
+                id = 3,
+                name = "Vegetable Stir Fry",
+                imageUri = R.drawable.chicken_curry
+            )
+        )
+
+        binding.recipesRecyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recipesRecyclerView.adapter = RecipeListAdapter(recipes)
     }
 }
